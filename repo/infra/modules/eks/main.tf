7c253e3d31729211a7a7ce3fff9d8fa205f2ce53
rishik50259 <rishik50259@gmail.com>
2026-04-22 18:32:34 +0530

    Modify KMS key policy for EKS secrets encryption
    Updated the KMS key policy to allow root permissions for the account. This change enhances security by specifying permissions for the encryption key used in EKS.

--- repo/infra/modules/eks/main.tf
+++ repo/infra/modules/eks/main.tf
@@ -122,6 +122,21 @@ resource "aws_kms_key" "eks" {
   deletion_window_in_days = 7
   enable_key_rotation     = true
 
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "EnableRootPermissions"
+        Effect = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      }
+    ]
+  })
+
   tags = {
     Name = "eks-secrets-encryption-key"
   }
